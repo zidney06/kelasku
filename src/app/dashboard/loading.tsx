a1161d0c2dc5f17acdc0e73ab7268330c250632e
zidney06 <zidniimportant@gmail.com>
2026-01-20 20:52:56 +0700

    Menambahkan animasi Loading... saat berganti halaman

---
+++ src/app/dashboard/loading.tsx
@@ -0,0 +1,12 @@
+export default function Loading() {
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="border rounded p-3 text-center w-100">
+        <h3 className="loading-dots">Loading</h3>
+      </div>
+    </div>
+  );
+}
